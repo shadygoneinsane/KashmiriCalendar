7f807eae9202a7c91d9c8befc77070e1f8556b70
vikeshdass <Findwork@007>
2020-04-20 10:12:33 +0530

    Removed unused files and updated gradle version

--- app/src/main/java/koushur/kashmirievents/database/entity/MonthDataEntity.kt
+++ app/src/main/java/koushur/kashmirievents/database/entity/MonthDataEntity.kt
@@ -2,13 +2,17 @@ package koushur.kashmirievents.database.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
 
 @Entity(tableName = "monthData")
 data class MonthDataEntity(
     @PrimaryKey
+    @SerializedName("date")
     val date: String,
 
+    @SerializedName("events")
     val events: String = "",
 
+    @SerializedName("imp")
     val imp: Int = 0
 )
